823703c811807440d6b7523c51bfc184b2df09e5
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-08-28 12:47:25 +0700

    update

--- Identifiers/Hi Low Issue/src/main/java/com/quangminh/hilowissue/entity/Author.java
+++ Identifiers/Hi Low Issue/src/main/java/com/quangminh/hilowissue/entity/Author.java
@@ -20,6 +20,17 @@ public class Author {
                     @Parameter(name = "optimizer", value = "hilo")
             }
     )
+
+    //hi-lo pooled
+//    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "hilopooled")
+//    @GenericGenerator(name = "hilopooled", strategy = "org.hibernate.id.enhanced.SequenceStyleGenerator",
+//            parameters = {
+//                    @Parameter(name = "sequence_name", value = "hilo_sequence"),
+//                    @Parameter(name = "initial_value", value = "1"),
+//                    @Parameter(name = "increment_size", value = "100"),
+//                    @Parameter(name = "optimizer", value = "pooled")
+//            }
+//    )
     @Column(name = "id", nullable = false)
     private Long id;
     private String name;
