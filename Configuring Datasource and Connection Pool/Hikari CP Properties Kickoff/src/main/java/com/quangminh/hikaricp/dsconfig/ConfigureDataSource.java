0a8748f08a47ed4102723847be5e1efe5074bd9c
QuangMinh1411 <102689124+QuangMinh1411@users.noreply.github.com>
2023-09-12 10:44:54 +0700

    Customize Datasoure

--- Configuring Datasource and Connection Pool/Hikari CP Properties Kickoff/src/main/java/com/quangminh/hikaricp/dsconfig/ConfigureDataSource.java
+++ Configuring Datasource and Connection Pool/Hikari CP Properties Kickoff/src/main/java/com/quangminh/hikaricp/dsconfig/ConfigureDataSource.java
@@ -10,17 +10,37 @@ import org.springframework.context.annotation.Primary;
 @Configuration
 public class ConfigureDataSource {
 
-    @Bean
-    @Primary
-    @ConfigurationProperties("app.datasource")
-    public DataSourceProperties dataSourceProperties() {
-        return new DataSourceProperties();
-    }
+//    @Bean
+//    @Primary
+//    @ConfigurationProperties("app.datasource")
+//    public DataSourceProperties dataSourceProperties() {
+//        return new DataSourceProperties();
+//    }
+//
+//    @Bean
+//    @ConfigurationProperties("app.datasource")
+//    public HikariDataSource dataSource(DataSourceProperties properties) {
+//        return properties.initializeDataSourceBuilder().type(HikariDataSource.class)
+//                .build();
+//    }
 
     @Bean
-    @ConfigurationProperties("app.datasource")
-    public HikariDataSource dataSource(DataSourceProperties properties) {
-        return properties.initializeDataSourceBuilder().type(HikariDataSource.class)
-                .build();
+    public HikariDataSource dataSource() {
+
+        HikariDataSource hds = new HikariDataSource();
+
+        hds.setJdbcUrl("jdbc:mysql://localhost:3306/numberdb?createDatabaseIfNotExist=true");
+        hds.setUsername("root");
+        hds.setPassword("Tom141117");
+        hds.setConnectionTimeout(50000);
+        hds.setIdleTimeout(300000);
+        hds.setMaxLifetime(900000);
+        hds.setMaximumPoolSize(8);
+        hds.setMinimumIdle(8);
+        hds.setPoolName("MyPool");
+        hds.setConnectionTestQuery("select 1 from dual");
+        hds.setAutoCommit(false);
+
+        return hds;
     }
 }
