a348a8388ba9eb0a8b46e4d2ac9ca3f6aaf858f7
Quang <tomminhquan1411@gmail.com>
2023-10-12 06:05:40 +0700

    Sub query in where

--- Queries/Sub Query in Where/Readme.md
+++ Queries/Sub Query in Where/Readme.md
@@ -8,3 +8,11 @@ Keep in mind that subqueries and joins queries may or may not be semantically eq
 Even if the Execution Plan is specific to the database, historically speaking joins are faster than subqueries among different databases, but this is not a rule (e.g., the amount of data may significantly influence the results). Of course, do not conclude that subqueries are just a replacement for joins that doesn't deserve attention. Tuning subqueries can increases their performance as well, but this is an SQL wide topic. So, benchmark! Benchmark! Benchmark!
 
 As a rule of thumb, prefer subqueries only if you cannot use joins, or if you can prove that they are faster than the alternative joins.
+
+How To Return A Map Result From A Spring Data Query Method
+
+Description: Sometimes, we need to write in repositories certain query-methods that return a Map instead of a List or a Set. For example, when we need a Map<Id, Entity> or we use GROUP BY and we need a Map<Group, Count>. This application shows you how to do it via default methods directly in repository.
+
+Key points:
+
+rely on default methods and Collectors.toMap()
--- Queries/Sub Query in Where/src/main/java/com/quangminh/subquerywhere/repository/AuthorRepository.java
+++ Queries/Sub Query in Where/src/main/java/com/quangminh/subquerywhere/repository/AuthorRepository.java
@@ -6,10 +6,35 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
+import java.util.Map;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public interface AuthorRepository extends JpaRepository<Author, Long> {
     @Transactional(readOnly = true)
     @Query("SELECT a FROM Author a WHERE a.id IN "
             + "(SELECT b.authorId FROM Bestseller b)")
     List<Author> fetchTheBest();
+
+    default Map<Long, Author> fetchIdAuthor() {
+
+        return findAll().stream()
+                .collect(Collectors.toMap(Author::getId, Function.identity()));
+    }
+
+    @Transactional(readOnly = true)
+    @Query("SELECT a.genre AS genre, count(a) AS genreCount FROM Author a GROUP BY a.genre")
+    List<Object[]> groupByGenreObj();
+
+    default Map<String, Long> groupByGenre() {
+
+        return groupByGenreObj()
+                .stream()
+                .collect(
+                        Collectors.toMap(
+                                t -> ((String) t[0]),
+                                t -> ((long) t[1])
+                        )
+                );
+    }
 }
