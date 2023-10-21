cb40ef665f2f0a952a901c4c21802ee2019364a2
Quang <tomminhquan1411@gmail.com>
2023-10-22 05:58:27 +0700

    Named queries via a properties file

---
+++ Queries/NamedQueriesInPropertiesFile/src/test/java/com/example/namedquerypf/NamedQueriesInPropertiesFileApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.namedquerypf;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NamedQueriesInPropertiesFileApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
